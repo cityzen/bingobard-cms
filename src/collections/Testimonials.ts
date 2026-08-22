0e3d08031e253e6936fac8d04792545f97e9ce46
Mike Hughes <mike@cityzen.com>
2026-08-22 18:56:01 -0400

    removed unused block reference

--- src/collections/Testimonials.ts
+++ src/collections/Testimonials.ts
@@ -1,24 +1,5 @@
 import type { CollectionConfig } from 'payload'
 
-const QuoteBlock: Block = {
-  slug: 'Quote', // required
-  imageURL: 'https://google.com/path/to/image.jpg',
-  imageAltText: 'A nice thumbnail image to show what this block looks like',
-  interfaceName: 'QuoteBlock', // optional
-  fields: [
-	// required
-	{
-	  name: 'quoteHeader',
-	  type: 'text',
-	  required: true,
-	},
-	{
-	  name: 'quoteText',
-	  type: 'text',
-	},
-  ],
-}
-
 export const Testimonials: CollectionConfig = {
 	  admin: {
 		useAsTitle: 'playerName',
